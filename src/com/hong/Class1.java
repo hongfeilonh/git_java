1d1205ecebac4b06471538ce99a03131934bb3b9
hong_fei_long <2314135571@qq.com>
2016-10-21 20:58:24 +0800

    第一次提交

---
+++ src/com/hong/Class1.java
@@ -0,0 +1,13 @@
+package com.hong;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("测试git");
+		System.out.println("测试git提交");
+		System.out.println("测试git远程提交");
+
+	}
+
+}
