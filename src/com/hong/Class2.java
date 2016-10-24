fe334063fd779e9fff1a8c0c1390cec7558fb321
hong_fei_long <2314135571@qq.com>
2016-10-24 17:18:47 +0800

    增加Class2需求

---
+++ src/com/hong/Class2.java
@@ -0,0 +1,14 @@
+package com.hong;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("增加class2需求");
+		System.out.println("增加class2需求");
+		System.out.println("增加class2需求");
+		System.out.println("增加class2需求");
+
+	}
+
+}
